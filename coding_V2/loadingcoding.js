bfbfb62010a701151c6fc21b00171a0d4d103a02
Tigreblack <lucas.dode26@gmail.com>
2025-03-26 17:23:06 +0100

    V7.03

--- coding_V2/loadingcoding.js
+++ coding_V2/loadingcoding.js
@@ -3,13 +3,14 @@
 document.addEventListener('DOMContentLoaded', function () {
     let progressBar = document.querySelector('.progress');
     let width = 0;
+
     let interval = setInterval(function () {
         if (width >= 100) {
             clearInterval(interval);
-            window.location.href = '../../../coding_v2/index.html';
+            window.location.href = 'index.html';
         } else {
             width++;
             progressBar.style.width = width + '%';
         }
-    }, 60); // Ajustez la vitesse de l'intervalle si nécessaire
+    }, 55); // Ajustez la vitesse de l'intervalle si nécessaire
 });
